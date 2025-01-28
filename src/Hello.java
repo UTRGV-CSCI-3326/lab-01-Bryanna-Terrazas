34c209db035c97d2e167f7fbc04612aac7b9da6f
Bryanna-Terrazas <bryanna.terrazas01@utrgv.edu>
2025-01-28 21:10:41 +0000

    Done

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,8 @@
 // Hello World
+public class Hello {
+    //creating work area
+    public static void main(String[] args){
+        //print out hello world
+        System.out.println("Hello World!");
+    }
+}
